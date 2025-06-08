baaa77927ca650336c5200b8de8251154cf062b0
flobobot <florian.laws@gmail.com>
2025-06-08 11:10:09 +0100

    Push

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("Portal loaded successfully.");
+
+function showModal(cocktail) {
+  document.getElementById('modal').classList.remove('hidden');
+  document.getElementById('modal-content').innerHTML = '<span onclick="closeModal()" class="close">&times;</span><h2>' + cocktail + '</h2><p>This is the full card for ' + cocktail + '.</p>';
+}
+function closeModal() {
+  document.getElementById('modal').classList.add('hidden');
+}
