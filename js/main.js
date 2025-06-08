f0944aae814bac78eaa74d7714bebb33bc49a292
flobobot <florian.laws@gmail.com>
2025-06-08 12:19:01 +0100

    Deploy

--- js/main.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('cocktail-list').innerText = 'Loading cocktails...';
--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-function showModal(cocktail) {
-  document.getElementById('modal').classList.remove('hidden');
-  document.getElementById('modal-content').innerHTML = '<span onclick="closeModal()" class="close">&times;</span><h2>' + cocktail + '</h2><p>This is the full card for ' + cocktail + '.</p>';
-}
-function closeModal() {
-  document.getElementById('modal').classList.add('hidden');
-}
